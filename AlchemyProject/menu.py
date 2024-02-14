2163bf5dc3082ea9737207eda6acb9e27c12d163
hazzaG <54991239+hazzaG@users.noreply.github.com>
2024-02-14 15:56:24 +0000

    Update 0.1

--- AlchemyProject/menu.py
+++ AlchemyProject/menu.py
@@ -2,7 +2,7 @@ import readData, addData, deleteData, validation
 
 readData.read()
 
-print("\nWhat would you like to do?\n3. Look at Potions\n")
+print("\nWhat would you like to do?\n3. Look at Potions\n4. Look at Ingredients\n")
 pathChoice = validation.validateInt()
 
 
@@ -13,46 +13,29 @@ def potionLook():
     potionType = validation.validateInt()
     readData.readPotions(potionRarity, potionType)
 
+#def ingredientTypeChooser(ingredientRole,ingredientTypeChoice)
+
 def ingredientLook():
-    print("\nWhat rarity of ingredients would you like to see?\n1. All\n2. Common\n")
+    print("\nWhat rarity of ingredients would you like to see?\n1. All\n2. Common\n3. Uncommon\n4. Rare\n5. Very rare\n")
     ingredientRarity = validation.validateInt()
     print("\nWhat role of ingredients would you like to see?\n1. All\n2. Tincture\n3. Solution\n4. Powders\n")
     ingredientRole = validation.validateInt()
     match ingredientRole:
         case 1:
-            ingredientType = 1
+            ingredientTypeChoice = 1
         case 2:
             print("\nWould you like to see Mushrooms or Roots?\n1. Mushrooms\n2. Roots\n3. Both\n")
-            ingredientTypeChoice = validation.validateInt
-            match ingredientTypeChoice:
-                case 1:
-                    ingredientType = 2
-                case 2:
-                    ingredientType = 3
-                case 3:
-                    ingredientType = 4
         case 3:
             print("\nWould you like to see Plants or Herbs?\n1. Plants\n2. Herbs\n3. Both\n")
-            ingredientTypeChoice = validation.validateInt
-            match ingredientTypeChoice:
-                case 1:
-                    ingredientType = 5
-                case 2:
-                    ingredientType = 6
-                case 3:
-                    ingredientType = 7
         case 4:
             print("\nWould you like to see Mushrooms or Roots?\n1. Bark\n2. Miscellaneous\n3. Both\n")
-            ingredientTypeChoice = validation.validateInt
-            match ingredientTypeChoice:
-                case 1:
-                    ingredientType = 8
-                case 2:
-                    ingredientType = 9
-                case 3:
-                    ingredientType = 10
-    readData.readIngredients(ingredientRarity, ingredientRole, ingredientType)
+    if ingredientRole != 1:
+        ingredientTypeChoice = validation.validateInt()
+    print(ingredientRarity, ingredientRole, ingredientTypeChoice)
+    readData.readIngredients(ingredientRarity, ingredientRole, ingredientTypeChoice)
 
 
 if pathChoice == 3:
     potionLook()
+if pathChoice == 4:
+    ingredientLook()
--- AlchemyProject/readData.py
+++ AlchemyProject/readData.py
@@ -62,12 +62,66 @@ if __name__ == "__main__":
 
 
 
-def readIngredients(ingredientRarity, ingredientRole, ingredientType):
+def readIngredients(ingredientRarity, ingredientRole, ingredientTypeChoice):
+    ingredientType = ingredientRole * (ingredientTypeChoice * ingredientRole)
+    print(ingredientType)
+
     match ingredientRarity:
-        case
+        case 1:
+            ingredientRarity = "%"
+        case 2:
+            ingredientRarity = "Common"
+        case 3:
+            ingredientRarity = "Uncommon"
+        case 4:
+            ingredientRarity = "Rare"
+        case 5:
+            ingredientRarity = "Very rare"
     
     match ingredientRole:
-        case
+        case 1:
+            ingredientRole = "%"
+        case 2:
+            ingredientRole = "Tincture"
+        case 3:
+            ingredientRole = "Solution"
+        case 4:
+            ingredientRole = "Powders"
     
     match ingredientType:
-        case
+        case 1:
+            ingredientType = "%"
+        case 4:
+            ingredientType = "Mushrooms"
+        case 8:
+            ingredientType = "Roots"
+        case 12:
+            ingredientType = "%"
+        case 9:
+            ingredientType = "Plants"
+        case 18:
+            ingredientType = "Herbs"
+        case 27:
+            ingredientType = "%"
+        case 16:
+            ingredientType = "Barks"
+        case 32:
+            ingredientType = "Miscellaneous"
+        case 48:
+            ingredientType = "%"
+    try:
+        dbCursor.execute(f"SELECT * FROM ingredients WHERE Rarity LIKE '{ingredientRarity}' AND IngRole LIKE '{ingredientRole}' AND IngType LIKE '{ingredientType}'")
+
+        rows = dbCursor.fetchall()
+        # OR 
+        # rows = dbCursor.execute("SELECT * FROM potions").fetchall()
+
+        if not rows:
+            print("No record exists")
+        else:
+            for aRecord in rows:
+                print(aRecord)
+    except sql.errors.Error as e:
+        print(f"{e}")
+if __name__ == "__main__":
+    readIngredients()
